3a0b22347e81202e09bb122ebd7a310b65cd32f1
SantuGamerReal <santubhaskar56@gmail.com>
2025-09-13 20:26:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/DoraemonSupportChat')  
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002950427887")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", -1002950427887")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS","-1002950427887")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
