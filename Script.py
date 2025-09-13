bf0b8aeab8de0634a3331e9b3805529ef48883ab
SantuGamerReal <santubhaskar56@gmail.com>
2025-09-13 20:09:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -216,7 +216,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/MissProviderBot">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/DoraemonRequestChat">[ Support Chat ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
